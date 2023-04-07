69d2a13086b7c60c16454b24df864551ceb0ff69
Angelo Troncone <angelotroncone@gmail.com>
2023-04-07 14:54:35 -0600

    Added getter/setters, insert, & search for BST

--- finalProj/dataStructures/nodes/TNode.py
+++ finalProj/dataStructures/nodes/TNode.py
@@ -1,11 +1,27 @@
 class TNode:
-    def __init__(self, data=None):
-        self.balance = 0
+
+    
+    def __init__(self, data=None, balance=None, P=None, L=None, R=None):
+        self.balance = balance
         self.left_height = 0
         self.right_height = 0
         
         self.data = data
-        self.left_node = None
-        self.right_node = None
-        self.parent_node = None
-        
+        self.left_node = L
+        self.right_node = R
+        self.parent_node = P
+
+    def set_data(self, new_data):
+        pass
+
+    def get_data(self):
+        pass
+    
+    # prints the node information to console in a user friendly format
+    def print(self, ):
+        pass
+    
+    # returns the data member as a string (will be used for the tree prints)
+    def toString(self):
+        pass
+
--- finalProj/dataStructures/trees/BST.py
+++ finalProj/dataStructures/trees/BST.py
@@ -15,28 +15,72 @@ class BSTree:
             self.root = data
 
     def set_root(self, data):
-        pass
+        if self.root == None:
+            if (type(data) == int):
+                self.root = TNode(data)
+            else:
+                self.root = data
+        else:
+            # TODO Must change the data field of the root
+            # and change the structure of the tree accordingly
+            pass
 
     def get_root(self):
-        pass
+        return self.root
 
     # If data is int, creates a new node with data val to be inserted into the tree
     # If data is TNode, inserts the node passed as argument into the tree
-    def insert(data):
+    def insert(self, data):
         if (type(data) == int):
-            pass
+            new_node = TNode(data)
         else:
-            pass
+            new_node = data
+        
+        parent_node = None
+        current_node = self.root
+        while current_node != None:
+            parent_node = current_node
+            if new_node.data <= current_node.data:
+                current_node = current_node.left_node
+            else:
+                current_node = current_node.right_node
+        
+        if self.root == None:
+            self.root = new_node
+        elif new_node.data <= parent_node.data:
+            parent_node.left_node = new_node
+        else:
+            parent_node.right_node = new_node
+        
+
     
     # finds the node with val as data and deletes it,
     # if not found prints a statement that the value is not in the tree
     def delete(self, value):
+        current_node = self.root
+        parent_node = None
+
+        while current_node != None:
+            parent_node = current_node
+            if value == current_node.data:
+                pass
+            elif value <= current_node.data:
+                current_node = current_node.left
         pass
 
     # searches for the node with val as data and returns it
     # or returns null if not found
     def search(self, value):
-        pass
+        current_node = self.root
+        while current_node != None:
+            parent_node = self.root
+            if value == parent_node.data:
+                return parent_node
+            elif value <= parent_node.data:
+                current_node = current_node.left_node
+            else:
+                current_node = current_node.right_node
+        return None
 
     # prints the content data of the tree in ascending order
     def print_in_order(self):
