f9c03a31add0d86489b2a074d126949f56943692
Angelo Troncone <angelotroncone@gmail.com>
2023-04-09 17:08:51 -0600

    Added skeleton code for AVL

--- finalProj/dataStructures/trees/AVL.py
+++ finalProj/dataStructures/trees/AVL.py
@@ -4,14 +4,38 @@ from BST import BST
 
 class AVLTree(BST):
     
-    def __init__(self, data):
-        self.balance = 0
-        self.left_height = 0
-        self.right_height = 0
-        
-        self.data = data
-        self.left_node = None
-        self.right_node = None
+    def __init__(self, data=None):
+        if data == None or type(data):
+            self.root = TNode(data)
+        else:
+            self.root = data
+
+    # Not complete, have to balance tree
+    def set_root(self, data):
+        super().set_root(data)
+
+    def get_root(self):
+        return self.root()
+    
+    # Not complete, have to balance tree
+    def insert(self, data):
+        super().insert(data)
+
+    # Not complete, have to balance tree
+    def delete(self, value):
+        super().delete(value)
+
+    def search(self, value):
+        return super().search(value)
+    
+    def print_in_order(self, current=True):
+        return super().print_in_order(current)
+    
+    def printBF(self):
+        super().printBF()
+    
+    
+
     
     
     
